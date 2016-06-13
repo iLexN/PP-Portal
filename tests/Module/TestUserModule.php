335daa72782e06e16e59d7543ec5e20ab3ce15c6
iLexN <ilex.job@gmail.com>
2016-06-13 16:45:01 +0800

    add test

---
+++ tests/Module/TestUserModule.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace PP\Test;
+
+class TestView extends \PHPUnit_Framework_TestCase
+{
+    private $c;
+    
+    public function testIsUserExistByID()
+    {
+        $userModule = new \PP\Module\UserModule($this->setUpContainer());
+
+        $this->assertTrue($userModule->isUserExistByID(1));
+
+        $this->assertEquals(1,$userModule->client->Client_NO);
+
+    }
+
+    public function testIsUserExistByIDWithCache()
+    {
+        $userModule = new \PP\Module\UserModule($this->setUpContainer());
+
+        
+
+        $this->assertTrue($userModule->isUserExistByID(1));
+
+        $this->assertEquals(1,$userModule->client->Client_NO);
+
+    }
+
+    public function testIsUserNotFind()
+    {
+        $userModule = new \PP\Module\UserModule($this->setUpContainer());
+
+        $this->assertFalse($userModule->isUserExistByID(100));
+    }
+    
+    public function setUpContainer()
+    {
+        $app = new \Slim\App();
+        $c = $app->getContainer();
+
+        $c['pool'] = function ($c) {
+            
+            $settings = [
+                'path' => __DIR__ . '/../cache/data',
+            ];
+
+            $driver = new \Stash\Driver\FileSystem($settings);
+
+            return new \Stash\Pool($driver);
+        };
+
+        $c['dataCacheConfig']= ['expiresAfter'=>3600];
+
+        return $c;
+
+    }
+    
+
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,14 @@ $autoloader->addPsr4('PP\Portal\Controller\\', __DIR__.'/../src/Controller');
 $autoloader->addPsr4('PP\Portal\dbModel\\', __DIR__.'/../src/DB-Model');
 $autoloader->addPsr4('PP\Module\\', __DIR__.'/../src/Module');
 $autoloader->addPsr4('PP\Middleware\\', __DIR__.'/../src/Middleware');
+
+$db = [
+         'driver'   => 'sqlite',
+         'database' => __DIR__.'/test.sqlite',
+         'prefix'   => '',
+    ];
+$capsule = new \Illuminate\Database\Capsule\Manager();
+$capsule->addConnection($db);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
