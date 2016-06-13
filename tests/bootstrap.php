8e6cdf48cce6560a91ea9b72b2f6b24914fcddf6
iLexN <ilex.job@gmail.com>
2016-06-13 12:32:52 +0800

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,7 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 $autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
+
 $autoloader->addPsr4('PP\Portal\Controller\\', __DIR__.'/../src/Controller');
 $autoloader->addPsr4('PP\Portal\dbModel\\', __DIR__.'/../src/DB-Model');
 $autoloader->addPsr4('PP\Module\\', __DIR__.'/../src/Module');
