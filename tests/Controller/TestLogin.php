110c744f8f83b9df5064595c916e5025c01ecca0
iLexN <ilex.job@gmail.com>
2016-06-17 13:53:38 +0800

    test login , try takeout TestUserModule

---
+++ tests/Controller/TestLogin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace PP\Test;
+
+class TestLogin extends \PHPUnit_Framework_TestCase
+{
+    private $c;
+
+    public function testCheckPlatformError()
+    {
+        $action = new \PP\Portal\Controller\User\Login($this->setUpContainer());
+
+        $_POST['clientID'] = '1';
+        $_POST['password'] = 'alex';
+        $environment = \Slim\Http\Environment::mock([
+                'REQUEST_METHOD' => 'POST',
+                'HTTP_CONTENT_TYPE' => 'multipart/form-data;'
+            ]);
+        $request = \Slim\Http\Request::createFromEnvironment($environment);
+        unset($_POST);
+
+        $response = new \Slim\Http\Response();
+
+        $response = $action($request, $response, []);
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode(['data' => [
+                'id' => 1,
+                ]]),
+                    json_encode(json_decode((string) $response->getBody()))
+        );
+    }
+    
+
+    public function setUpContainer()
+    {
+        $c = new \Slim\Container();
+
+        $c['jsonConfig'] = ['prettyPrint' => false];
+
+        $c['ViewHelper'] = function ($c) {
+            return new \PP\Portal\Module\Helper\View($c);
+        };
+
+        $c['UserModule'] = function($c){
+            return new \PP\Portal\Module\UserModule($c);
+        };
+
+        $c['pool'] = function ($c) {
+            $settings = [
+                'path' => __DIR__.'/../cache/data',
+            ];
+
+            $driver = new \Stash\Driver\FileSystem($settings);
+
+            return new \Stash\Pool($driver);
+        };
+
+        $c['dataCacheConfig'] = ['expiresAfter' => 3600];
+
+        return $c;
+    }
+}
---
+++ tests/Module/TestFileUploadModule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PP\Test;
+
+class TestFileUploadModule extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+       $attr = [
+            'tmp_name' => '.abc123',
+            'name'     => 'my-avatar.txt',
+            'size'     => 8,
+            'type'     => 'text/plain',
+            'error'    => 0,
+        ];
+        $uploadedFile = new \Slim\Http\UploadedFile(
+            $attr['tmp_name'],
+            $attr['name'],
+            $attr['type'],
+            $attr['size'],
+            $attr['error'],
+            false
+        );
+
+        $uploadModule = new \PP\Portal\Module\FileUploadModule($uploadedFile);
+
+        $this->assertEquals($attr['name'], $uploadModule->getClientFilename());
+        //$this->assertEquals($attr['type'], $uploadModule->getClientMediaType());
+        //$this->assertEquals($attr['size'], $uploadModule->getSize());
+        $this->assertEquals($attr['error'], $uploadModule->getError());
+
+    }
+
+    
+}
