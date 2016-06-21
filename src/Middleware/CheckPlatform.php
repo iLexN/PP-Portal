713550e9f1ef8e5610077d7f99d63b409be9b836
iLexN <ilex.job@gmail.com>
2016-06-21 09:40:14 +0800

    cs fix

--- src/Middleware/CheckPlatform.php
+++ src/Middleware/CheckPlatform.php
@@ -56,9 +56,9 @@ class CheckPlatform extends AbstractContainer
     private function sendGaEvent()
     {
         $httpClient = $this->c['httpClient'];
-        $response = $httpClient->post('https://www.google-analytics.com/debug/collect',[
-            'body'=>'v=1&t=event&tid=UA-5195172-41&cid=e4c2eec3-3834-4992-81ef-1e615c0e7177&ec=API&ea=Call&el=%7B%7BrouteName%7D%7D'
-        ]);
+        $response = $httpClient->post('https://www.google-analytics.com/debug/collect', [
+            'body' => 'v=1&t=event&tid=UA-5195172-41&cid=e4c2eec3-3834-4992-81ef-1e615c0e7177&ec=API&ea=Call&el=%7B%7BrouteName%7D%7D',
+         ]);
         $log = [
                 'getStatusCode' => $response->getStatusCode(),
                 'body'          => (string) $response->getBody(),
