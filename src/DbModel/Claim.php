67136e7bbc5324cee5531a5097f50e0e98a8e77f
iLexN <ilex.job@gmail.com>
2016-08-10 14:39:44 +0800

    amount => double

--- src/DbModel/Claim.php
+++ src/DbModel/Claim.php
@@ -18,6 +18,10 @@ class Claim extends Model
 
     protected $fillable = ['user_policy_id', 'currency', 'amount', 'date_of_treatment', 'diagnosis', 'payment_method', 'issue_to_whom', 'issue_address', 'currency_receive', 'status'];
 
+    protected $casts = [
+        'amount' => 'double',
+    ];
+
     public function fileAttachments()
     {
         return $this->hasMany(__NAMESPACE__.'\ClaimFile', 'claim_id');
