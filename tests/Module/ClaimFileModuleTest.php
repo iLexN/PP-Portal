6e04acd6e2453fc6ff0454d216a03b32821591fc
iLexN <ilex.job@gmail.com>
2017-03-16 10:17:07 +0800

    fix test

--- tests/Module/ClaimFileModuleTest.php
+++ tests/Module/ClaimFileModuleTest.php
@@ -21,7 +21,7 @@ class ClaimFileModuleTest extends \PHPUnit\Framework\TestCase
         };
         $c['uploadConfig'] = function () {
             return [
-                'path'          => __DIR__.'/../../cache',
+                'path'          => __DIR__.'/../cache',
                 'client_upload' => '/claim_upload/',
             ];
         };
